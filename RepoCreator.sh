1f9eae0d3c42b406145200053722a617913c302b
vlad29 <xbian@xbian.private>
2014-11-10 23:24:23 +0100

    Add code of the RepoCreator

---
+++ RepoCreator.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# Creator of Repos by vlad29
+#EG: INITIAL_ROOT=/home/username/repositories
+INITIAL_ROOT=
+#EG: REPOSERVER=serverpath.com
+REPOSERVER=
+#EG: REPOUSER=username
+REPOUSER=
+
+echo 'Write the name of the new repo:'
+read REPONAME
+
+if [[ -z "$REPONAME" ]];
+then
+let REPONAME=$(date "+%Y%m%d%H%M%S")
+fi
+
+REPONAME="$REPONAME.git"
+
+cd $INITIAL_ROOT
+mkdir ./$REPONAME
+cd ./$REPONAME/
+git init --bare
+
+
+#echo '"git clone ssh://git@server/path/to/git-repo"
+
+#echo "git clone ssh://$REPOUSER@$REPOSERVER$INITIAL_ROOT/$REPONAME"
+echo "__________________________________________________________"
+echo ""
+echo "                    $REPONAME                      "
+echo "__________________________________________________________"
+echo "__________________EMPTY__PROJECT__________________________"
+echo " cd my_project"
+echo " git init"
+echo " git add *"
+echo " git commit -m \"My initial commit message\" "
+echo " git remote add origin ssh://$REPOUSER@$REPOSERVER$INITIAL_ROOT/$REPONAME" 
+echo " git push -u origin master"
+echo "__________________________________________________________"
+echo "________________CLONE__PROJECT____________________________"
+echo "git clone ssh://$REPOUSER@$REPOSERVER$INITIAL_ROOT/$REPONAME" 
+
+exit 0
